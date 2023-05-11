7b10193e82d8f34baa6a7b137a99810c7cf55913
wilsonhouston <wasasman01@gmail.com>
2023-05-11 18:05:21 -0500

    Learning Activity - C# Prep 4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,56 @@ class Program
 {
     static void Main(string[] args)
     {
+        //using System.Collections.Generic;
+
+        List<int> numbers = new List<int>();
+
+        int Unumber = -1;
+
+        while (Unumber != 0)
+        {
+
+            Console.Write("Enter a number: ");
+            string userInput = Console.ReadLine();
+            Unumber = int.Parse(userInput);
+
+            if (Unumber != 0)
+            {
+                numbers.Add(Unumber);
+            }
+        }
+
+        int sum = 0;
+
+        foreach (int number in numbers)
+        {
+           sum += number;
+        }
+
+        int length = numbers.Count;
+
+        float average = ((float)sum) / length;
+
+        int max = numbers[0];
+
+        foreach (int number in numbers)
+        {
+            if (max > number)
+            {
+                max = number;
+            }
+        }
+
+
+        Console.WriteLine($"The sum is: {sum}");
+        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"The max number is {max}");
+
+
+
+
+
+
         Console.WriteLine("Hello Prep4 World!");
     }
 }
